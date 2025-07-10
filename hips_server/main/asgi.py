7bafead94412de9f10528edcdeb87c9e1048f410
Anne Haley <anne.haley@kitware.com>
2025-07-10 15:09:32 -0400

    Replace instances of old directory names

--- hips_server/main/asgi.py
+++ hips_server/main/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for atlascope_django project.
+ASGI config for hips_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- hips_server/main/wsgi.py
+++ hips_server/main/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for atlascope_django project.
+WSGI config for hips_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atlascope_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hips_server.settings')
 
 application = get_wsgi_application()
