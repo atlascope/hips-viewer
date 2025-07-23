4c0363e9bc161ce001e18f71e7dbdc1c2d070c3b
Anne Haley <anne.haley@kitware.com>
2025-07-23 15:56:38 -0400

    Fix cell drawer infinite scroll end behavior

--- hips_viewer/src/CellDrawer.vue
+++ hips_viewer/src/CellDrawer.vue
@@ -13,23 +13,27 @@ const pageLength = 10;
 const thumbnails = ref<Thumbnail[]>([]);
 
 function loadThumbnails({ done }: any) {
-    thumbnails.value.push(...props.cells.slice(
-        thumbnails.value.length, thumbnails.value.length + pageLength
-    ).map((cell) => {
-        const region = new URLSearchParams({
-            left: (cell.x - cell.width / 2).toString(),
-            right: (cell.x + cell.width / 2).toString(),
-            top: (cell.y - cell.height / 2).toString(),
-            bottom: (cell.y + cell.height / 2).toString(),
-        })
-        return {
-            src: `${props.tile_url}/region/?${region}`,
-            width: cell.width,
-            height: cell.height,
-        }
-    }))
-    if (thumbnails.value.length) done('ok')
-    else done('empty')
+    if (props.cells.length === thumbnails.value.length) done('empty')
+    else {
+        thumbnails.value.push(...props.cells.slice(
+            thumbnails.value.length, thumbnails.value.length + pageLength
+        ).map((cell) => {
+            const region = new URLSearchParams({
+                left: (cell.x - cell.width / 2).toString(),
+                right: (cell.x + cell.width / 2).toString(),
+                top: (cell.y - cell.height / 2).toString(),
+                bottom: (cell.y + cell.height / 2).toString(),
+            })
+            return {
+                src: `${props.tile_url}/region/?${region}`,
+                width: cell.width,
+                height: cell.height,
+                id: cell.id,
+            }
+        }))
+        if (thumbnails.value.length) done('ok')
+        else done('empty')
+    }
 }
 </script>
 
@@ -38,7 +42,7 @@ function loadThumbnails({ done }: any) {
         :items="thumbnails"
         :height="props.height"
         @load="loadThumbnails"
-        empty-text="End of selected cells"
+        empty-text="End of cell list"
     >
         <div class="cell-thumbnail-container">
             <img
