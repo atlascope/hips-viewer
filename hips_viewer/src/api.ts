db229038e3618322bc218e6e40d42880d0f7a6ac
Anne Haley <anne.haley@kitware.com>
2025-07-23 10:39:07 -0400

    Implement a basic cache for fetching cell data

--- hips_viewer/src/api.ts
+++ hips_viewer/src/api.ts
@@ -1,15 +1,26 @@
-import axios from 'axios';
 import type { Cell } from './types';
 
 
 export const baseURL = 'http://localhost:8000/api';
 
-export const apiClient = axios.create({
-  baseURL,
-});
+export async function cachedFetch(url: string, cacheName: string) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(url);
+  let data;
+  if (cachedResponse) {
+    data = await cachedResponse.json()
+  } else {
+    const response = await fetch(url)
+    // Clone the response because a Response object can only be consumed once
+    await cache.put(url, response.clone())
+    data = await response.json()
+  }
+  return data
+}
 
 export async function fetchImages() {
-  return (await apiClient.get('images')).data
+  const url = `${baseURL}/images`
+  return (await fetch(url)).json()
 }
 
 export async function fetchImageCells(imageId: number) {
@@ -17,9 +28,8 @@ export async function fetchImageCells(imageId: number) {
   let results: Cell[] = [];
   let total: number | undefined = undefined;
   while (!total || results.length < total) {
-    const response: {items: Cell[], count: number} = (
-      await apiClient.get(`images/${imageId}/cells?offset=${results.length}&limit=${limit}`)
-    ).data
+    const url = `${baseURL}/images/${imageId}/cells?offset=${results.length}&limit=${limit}`
+    const response: {items: Cell[], count: number} = await cachedFetch(url, 'cell-data-cache')
     if (!total) total = response.count
     results = [...results, ...response.items]
   }
